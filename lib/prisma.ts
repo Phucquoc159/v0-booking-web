04355a8f4ba9ccf6688910fec0cd95b32cd7ff4a
rolniuq <truongquynh2525@gmail.com>
2025-10-26 11:10:53 +0700

    chore: update lib/prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma;
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-if ((process.env.NODE_ENV = "production")) {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  })
 
-  prisma = global.prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma;
+export default prisma
