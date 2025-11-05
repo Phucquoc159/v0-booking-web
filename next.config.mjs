727ea949e8b3a772968fd01232e19b47f042da43
Phucquoc159 <65392634+Phucquoc159@users.noreply.github.com>
2025-11-05 22:50:03 +0700

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-  },
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000, // Check for changes every second
-        aggregateTimeout: 300, // Delay before rebuilding
-      }
-    }
-    return config
   },
 }
 
