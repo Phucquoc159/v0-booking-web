f02efd1026c98ccdc19fb6efb19d694e70bc3619
rolniuq <truongquynh2525@gmail.com>
2025-11-12 23:28:04 +0700

    chore: update script

--- testdata/nextjs_db.sql
+++ testdata/nextjs_db.sql
@@ -6,7 +6,11 @@
 -- Database: nextjs_db
 -- Generation Time: 2025-11-06 23:31:43.9000
 -- -------------------------------------------------------------
+INSERT INTO "kieu_phong" ("ID_KP", "TEN_KP", "MO_TA", "SO_LUONG_KHACH") VALUES
+('KP1', 'VIP 1', 'VIP', 10);
 
+INSERT INTO "loai_phong" ("ID_LP", "TEN_LP", "MO_TA") VALUES
+('LP1', 'VIP 1', 'VIP');
 
 INSERT INTO "bo_phan" ("ID_BP", "TEN_BP") VALUES
 ('BP1', 'Le tan');
@@ -14,11 +18,6 @@ INSERT INTO "bo_phan" ("ID_BP", "TEN_BP") VALUES
 INSERT INTO "hang_phong" ("ID_HANG_PHONG", "ID_KP", "ID_LP") VALUES
 ('HP1', 'KP1', 'LP1');
 
-INSERT INTO "kieu_phong" ("ID_KP", "TEN_KP", "MO_TA", "SO_LUONG_KHACH") VALUES
-('KP1', 'VIP 1', 'VIP', 10);
-
-INSERT INTO "loai_phong" ("ID_LP", "TEN_LP", "MO_TA") VALUES
-('LP1', 'VIP 1', 'VIP');
 
 INSERT INTO "nhan_vien" ("ID_NV", "HO", "TEN", "PHAI", "NGAY_SINH", "DIA_CHI", "SDT", "EMAIL", "HINH", "USERNAME", "PASSWORD", "ID_BP", "ID_NQ") VALUES
 ('NV1', 'Quoka', 'Ka', 'NAM', '2025-10-27', 'Test', '0901111111', 'admin@qkhotel.com', NULL, 'admin@qkhotel.com', 'test', '1', 'NQ1'),
