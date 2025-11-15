ba6b00c298c992e25b3dde9ace725b8735e14a47
Alan Vo <alan.vo.asiabots.com>
2025-11-15 13:36:23 +0700

    Update check auth frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,32 @@
+"use client"
 import type React from "react"
-import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { Suspense } from "react"
 import Providers from "./providers"
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { usePathname } from "next/navigation"
 
-export const metadata: Metadata = {
-  title: "QK Hotel - Khách Sạn Sang Trọng & Hiện Đại",
-  description: "Đặt phòng khách sạn QK Hotel - Trải nghiệm dịch vụ 5 sao với giá tốt nhất",
-  generator: "v0.app",
-}
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const router = useRouter();
+  const pathname = usePathname();
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("userData") !== null;
+    const routerNoCheck = ["/login", "/register", '/'].includes(pathname);
+    console.log(!isLoggedIn && !routerNoCheck);
+    if (!isLoggedIn && !routerNoCheck) {
+      localStorage.removeItem("userData");
+      router.push('/login');
+    }
+  }, [pathname, router])
   return (
     <html lang="vi">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/components/header"
 import { HeroBanner } from "@/components/hero-banner"
+import type { Metadata } from "next"
 import { Amenities } from "@/components/amenities"
 import { RoomSuggestions } from "@/components/room-suggestions"
 import { Reviews } from "@/components/reviews"
@@ -8,6 +9,11 @@ import { LocationMap } from "@/components/location-map"
 import { Footer } from "@/components/footer"
 import { ChatBot } from "@/components/chatbot"
 
+export const metadata: Metadata = {
+  title: "QK Hotel - Khách Sạn Sang Trọng & Hiện Đại",
+  description: "Đặt phòng khách sạn QK Hotel - Trải nghiệm dịch vụ 5 sao với giá tốt nhất",
+  generator: "v0.app",
+}
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
